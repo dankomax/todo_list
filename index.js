8c3f3b74077882a1c3fa42346017fea8537d8fe1
DankoMaksym <dankomaksym@gmail.com>
2019-12-20 23:46:49 +0200

    remove js and css from public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/server'));
+app.use(express.static(__dirname + '/app'));
 
 app.listen(port);
 console.log(`working on ${port}`);
